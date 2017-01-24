a869ebac26325f154e9f51b6011736fab771f71c
Li Lin <guru.lin@gmail.com>
2017-01-23 20:57:10 -0800

    added react-native-push-notification.

--- android/app/src/main/java/com/breezex/MainApplication.java
+++ android/app/src/main/java/com/breezex/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import com.auth0.lock.react.LockReactPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new RNDeviceInfo(),
             new GoogleAnalyticsBridgePackage(),
             new LockReactPackage(),
