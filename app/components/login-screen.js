119c4aa31fcd488f790f76fcf73c69756c63a4c6
Li Lin <guru.lin@gmail.com>
2017-01-23 20:58:53 -0800

    Revert "added logic to send device info to server"
    This reverts commit 70c8360da6951133b40780b46959b9e37925ed13.
    Removed logic for sending device uuid to server.

--- app/components/login-screen.js
+++ app/components/login-screen.js
@@ -3,43 +3,15 @@ import {
   Text,
   View,
   TouchableHighlight,
-  AsyncStorage,
-  Alert
+  AsyncStorage
 } from 'react-native';
 import Auth0Lock from 'react-native-lock';
-import DeviceInfo from 'react-native-device-info';
 
 import env from '../config/environment';
 import styles from '../styles';
 import { tracker } from '../analytics';
 
 const lock = new Auth0Lock({ clientId: env.AUTH0_CLIENT_ID, domain: env.AUTH0_DOMAIN });
-
-const registerDevice = (token) => {
-  fetch(`${env.API_URL}/device_registration`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-      Accept: 'applicaiton/json'
-    },
-    body: JSON.stringify({
-      device_uuid: DeviceInfo.getUniqueID(),
-      model: DeviceInfo.getModel(),
-      device_id: DeviceInfo.getDeviceId()
-    })
-  })
-    .then(() => { console.log('registered device with server'); })
-    .catch(() => {
-      Alert.alert(
-        'Registration Failed',
-        'Unable to register this device with the server',
-        [
-          { text: 'OK' },
-        ]
-      );
-    });
-};
 
 export default class LoginScreen extends Component {
 
@@ -56,7 +28,6 @@ export default class LoginScreen extends Component {
         console.log(err);
         return;
       }
-      registerDevice(token.idToken);
 
       AsyncStorage.multiSet([
         ['AUTH0-PROFILE', JSON.stringify(profile)],
@@ -67,11 +38,11 @@ export default class LoginScreen extends Component {
         () => console.log('cannot storage token to storage!')
         );
     });
-  };
+  }
 
   render() {
     return (
-      <View style={styles.container} >
+      <View style={styles.container}>
         <View style={styles.messageBox}>
           <Text style={styles.title}>Fictional Bank</Text>
           <Text style={styles.subtitle}>Where all your money belong</Text>
@@ -83,7 +54,7 @@ export default class LoginScreen extends Component {
         >
           <Text>Log In</Text>
         </TouchableHighlight>
-      </View >
+      </View>
     );
   }
 }
